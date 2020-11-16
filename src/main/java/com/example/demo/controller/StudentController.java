a28a432eb01ab54db95728a55722b03c6b8d359c
Innocent-Mbanda <mbandainnocent@gmail.com>
2020-11-16 12:29:54 -0500

    add orign and route headers

--- src/main/java/com/example/demo/controller/StudentController.java
+++ src/main/java/com/example/demo/controller/StudentController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.Optional;
-
+@CrossOrigin(origins = "*", allowedHeaders = "*")
 @RestController
 public class StudentController {
   @Autowired
